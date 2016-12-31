c72fdd9d356409354d164220c14639684de72b33
Ajeya Rengarajan <ajrengar@ucsd.edu>
2016-12-31 15:05:04 +0530

    initial commit

---
+++ Pro Tip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pro Tip
+//
+//  Created by Ajeya Rengarajan on 12/31/16.
+//  Copyright © 2016 Ajeya Rengarajan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
