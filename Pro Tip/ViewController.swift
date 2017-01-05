7ec5c04970f6c2d236dd387214f8b9f89cc812f9
Ajeya Rengarajan <ajrengar@ucsd.edu>
2017-01-05 22:56:32 +0530

    functional

--- Pro Tip/ViewController.swift
+++ Pro Tip/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var amountText: UITextField!
+    @IBOutlet weak var tipSelector: UISegmentedControl!
+    
+    var tipPercent: Double = 0.15
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,34 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func tipChanged(_ sender: AnyObject) {
+        let currentIndex = tipSelector.selectedSegmentIndex;
+        
+        var title: String = tipSelector.titleForSegment(at: currentIndex)!
+        
+        print (title)
+        title.remove(at: title.index(before: title.endIndex))
+        
+        print (title)
+        
+        tipPercent = Double (title) ?? 0
+        tipPercent = tipPercent * 0.01
+        onAmountChange(sender);
+    }
 
+    @IBAction func onAmountChange(_ sender: AnyObject) {
+        
+        let amount = Double(amountText.text!) ?? 0
+        let tip = tipPercent * amount
+        let total = amount + tip;
+        
+        tipLabel.text = String (format: "$%.2f", tip)
+        totalLabel.text = String (format: "$%.2f", total)
+    }
+    
+    @IBAction func onTap(_ sender: Any) {
+        
+        view.endEditing(true)
+    }
 }
 
